d0aea383f9608f95e11c606c00272d5558941140
ERussel <emkil.russel@gmail.com>
2025-06-25 17:27:07 +0200

    fix build

--- bindings/xcframework/raptorq.xcframework/macos-arm64/raptorq.framework/Headers/raptorq.h
+++ bindings/xcframework/raptorq.xcframework/macos-arm64/raptorq.framework/Headers/raptorq.h
@@ -1,8 +1,17 @@
-#include <cstdarg>
-#include <cstdint>
-#include <cstdlib>
-#include <new>
+#ifndef __RAPTORQ_INCLUDE_GUARD_H__
+#define __RAPTORQ_INCLUDE_GUARD_H__
 
-static const uint32_t MAX_SOURCE_SYMBOLS_PER_BLOCK = 56403;
+/* Generated with cbindgen:0.14.3 */
 
-static const uint32_t SPARSE_MATRIX_THRESHOLD = 250;
+/* THIS FILE WAS AUTOMATICALLY GENERATED. DO NOT EDIT.*/
+
+#include <stdarg.h>
+#include <stdbool.h>
+#include <stdint.h>
+#include <stdlib.h>
+
+#define MAX_SOURCE_SYMBOLS_PER_BLOCK 56403
+
+#define SPARSE_MATRIX_THRESHOLD 250
+
+#endif /* __RAPTORQ_INCLUDE_GUARD_H__ */
